407766ee2adee19c37ec4141105e27b41e22c967
ankurvaseashta <root@USD1703230.local>
2018-08-11 17:49:21 -0400

    Passing Method References Between Components

--- src/App.js
+++ src/App.js
@@ -12,12 +12,12 @@ class App extends Component {
     otherState: 'some other value'
   }
 
-switchNameHandler = () => {
+switchNameHandler = ( newName ) => {
   //console.log('Was clicked!')
   // DON'T DO THIS: this.state.persons[0].name = "AnkurVaseashta";
   this.setState({
     persons: [
-     { name: 'Ankur', age: 33 },
+     { name: newName, age: 33 },
      { name: 'Helena', age: 30 },
      { name: 'Buggy', age: 15 }
     ]
@@ -29,11 +29,17 @@ switchNameHandler = () => {
       <div className="App">
         <h1> Hi, I am a react app!! </h1>
         <p> This is really working! </p>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
-
+        <button onClick={() => this.switchNameHandler('Ankura!!!!')}>Switch Name</button>
+        <Person 
+          name={this.state.persons[0].name} 
+          age={this.state.persons[0].age}/>
+        <Person 
+          name={this.state.persons[1].name} 
+          age={this.state.persons[1].age}
+          click={this.switchNameHandler.bind(this, 'Ankur!!')}> My Hobbies: Coding</Person>
+        <Person 
+          name={this.state.persons[2].name} 
+          age={this.state.persons[2].age}/>
       </div>
     );
     ///*return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, I am a react app!!!'));*/
