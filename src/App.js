bbaf380a303a7fe92f8a6b18c68325d35ecd17a7
ankurvaseashta <root@USD1703230.local>
2018-08-12 18:17:22 -0400

    Flexible lists rendering

--- src/App.js
+++ src/App.js
@@ -20,7 +20,24 @@ deletePersonHandler = (personIndex) => {
   this.setState({persons: persons})
 }
 
-nameChangedHandler = (event) => {
+nameChangedHandler = (event, id) => {
+  const personIndex = this.state.persons.findIndex(p => {
+    return p.id === id;
+  });
+
+  const person = {
+    ...this.state.persons[personIndex]
+  };
+
+  //const person Object.assign({}, this.state.persons[personIndex]);
+
+  person.name = event.target.value;
+
+  const persons = [...this.state.persons];
+  persons[personIndex] = person;
+
+  this.setState( {persons: persons} );
+
   this.setState({
     persons: [
      { name: 'Ankur', age: 53 },
@@ -55,7 +72,8 @@ togglePersonsHandler = () => {
               click={() => this.deletePersonHandler(index)}
               name={person.name} 
               age={person.age} 
-              key={person.id} />
+              key={person.id}
+              changed={(event) => this.nameChangedHandler(event, person.id)} />
             })}
           </div> 
         );
