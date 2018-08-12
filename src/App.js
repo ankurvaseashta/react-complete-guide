c9b2b64a2dd35020d8397a3d077a75b10af18730
ankurvaseashta <root@USD1703230.local>
2018-08-12 17:55:56 -0400

    Create copy of a persons array using slice method or use spread operator for the object copy

--- src/App.js
+++ src/App.js
@@ -13,16 +13,11 @@ class App extends Component {
     showPersons: false
   }
 
-switchNameHandler = (newName) => {
-  //console.log('Was clicked!')
-  // DON'T DO THIS: this.state.persons[0].name = "AnkurVaseashta";
-  this.setState({
-    persons: [
-     { name: newName, age: 33 },
-     { name: 'Helena', age: 30 },
-     { name: 'Buggy', age: 15 }
-    ]
-  } )
+deletePersonHandler = (personIndex) => {
+  //const persons = this.state.persons.slice();
+  const persons = [...this.state.persons];
+  persons.splice(personIndex, 1);
+  this.setState({persons: persons})
 }
 
 nameChangedHandler = (event) => {
@@ -55,8 +50,9 @@ togglePersonsHandler = () => {
     if(this.state.showPersons){
       persons = (
             <div>
-            {this.state.persons.map(person => {
+            {this.state.persons.map((person, index) => {
               return <Person 
+              click={() => this.deletePersonHandler(index)}
               name={person.name} 
               age={person.age} />
             })}
