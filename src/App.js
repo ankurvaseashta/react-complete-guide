c80ddb9478179eda14a153b03f57aeb96396488f
ankurvaseashta <root@USD1703230.local>
2018-08-12 17:46:03 -0400

    Outputting lists

--- src/App.js
+++ src/App.js
@@ -55,17 +55,11 @@ togglePersonsHandler = () => {
     if(this.state.showPersons){
       persons = (
             <div>
-            <Person 
-              name={this.state.persons[0].name} 
-              age={this.state.persons[0].age}/>
-            <Person 
-              name={this.state.persons[1].name} 
-              age={this.state.persons[1].age}
-              click={this.switchNameHandler.bind(this, 'Ankur!!')}
-              changed={this.nameChangedHandler} >My Hobbies: Coding</Person>
-            <Person 
-              name={this.state.persons[2].name} 
-              age={this.state.persons[2].age}/>
+            {this.state.persons.map(person => {
+              return <Person 
+              name={person.name} 
+              age={person.age} />
+            })}
           </div> 
         );
     }
