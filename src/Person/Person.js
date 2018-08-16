8d734b7408d6050a8a4d6c65bcdaff717068c186
ankurvaseashta <root@USD1703230.local>
2018-08-16 13:44:24 -0400

    Lesson 71: Using CSS modules along with Media Queries

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -4,7 +4,7 @@ import classes from './Person.css';
 const person = ( props ) => { 
 
   return (
-    <div className={ classes.Person }>
+    <div className={classes.Person}>
         <p onClick={props.click}>I am {props.name} a person and I am {props.age} years old! </p>
         <p> {props.children}</p>
         <input type="text" onChange={props.changed} value={props.name} />
