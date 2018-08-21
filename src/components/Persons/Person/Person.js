f4eaf9c014b85bb6ee67de6a4eb407e22653ff9c
ankurvaseashta <root@USD1703230.local>
2018-08-21 08:40:38 -0400

    Lesson 79: A better project structure

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -3,17 +3,11 @@ import classes from './Person.css';
 
 const person = ( props ) => { 
 
-  const rnd = Math.random();
-
-  if ( 0.6 === 0.7 ){
-  	throw new Error('Something went wrong!');
-  }
 
   return (
-
     <div className={classes.Person}>
         <p onClick={props.click}>I am {props.name} a person and I am {props.age} years old! </p>
-        <p> {props.children}</p>
+        <p> {props.children} </p>
         <input type="text" onChange={props.changed} value={props.name} />
     </div> 
   )
