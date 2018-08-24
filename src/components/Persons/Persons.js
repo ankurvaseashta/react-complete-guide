d016b983053eb8addcda35739b0ef265bb095c01
ankurvaseashta <root@USD1703230.local>
2018-08-24 08:52:03 -0400

    Lessons 83 and 84: converting stateless to stateful components and understand lifecycle

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Person from './Person/Person';
 
 /* Default ES6 Arrow Function Notation */
-const persons = (props) => 	props.persons.map( (person, index) => { 
+class Persons extends Component {
+	render(){
+		return this.props.persons.map( (person, index) => { 
+
           return <Person  
-              click={() => props.clicked(index)}
+              click={() => this.props.clicked(index)}
               name={person.name} 
               age={person.age}
               key={person.id} 
-              changed={(event) => props.changed(event, person.id)} />
+              changed={(event) => this.props.changed(event, person.id)} />
             } );
+	}
 
-export default persons;
+} 
+
+export default Persons;
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+
 import classes from './App.css';
 import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
