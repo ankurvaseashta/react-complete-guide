c839d29fac665c4a098b38295c2ba67af010c2de
ankurvaseashta <root@USD1703230.local>
2018-08-27 10:07:01 -0400

    Lesson 87 Update component lifecycle using hooks

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -2,10 +2,9 @@ import React, { Component } from 'react';
 import classes from './Person.css';
 
 class Person extends Component {
-	
 	constructor(props){
-    super(props);
-    console.log('[Person.js] Inside Constructor', props);
+	super(props);
+	console.log('[Person.js] Inside Constructor', props);
   }
 
   componentWillMount(){
--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -16,10 +16,26 @@ class Persons extends Component {
 
   componentDidMount(){
     console.log('[Persons.js] Inside componentDidMount()');
+  }//not called through DOM events
+
+  componentWillReceiveProps(nextProps){
+    console.log('[UPDATE Persons.js] Inside componentWillReceiveProps', nextProps);
   }
 
+  shouldComponentUpdate(nextProps, nextState){
+    console.log('[UPDATE Persons.js] Inside shouldComponentUpdate', nextProps, nextState);
+    return nextProps.persons !== this.props.persons;
+  }
 
-	render(){
+  componentWillUpdate(nextProps, nextState) {
+    console.log('[UPDATE Persons.js] Inside componentWillUpdate', nextProps, nextState);
+  }
+
+  componentDidUpdate(){
+    console.log('[UPDATE Persons.js] Inside componentDidUpdate');
+  }
+
+	render() {
     console.log('[Persons.js] Inside render()');
 		return this.props.persons.map( (person, index) => { 
           return <Person  
