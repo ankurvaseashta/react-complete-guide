d1a09f933312bf25e38f7df7b00349fc3f0f0e2b
ankurvaseashta <root@USD1703230.local>
2018-08-29 11:03:01 -0400

    Make hoc component withClass upper case WithClass JS file

--- src/hoc/WithClass.js
+++ src/hoc/WithClass.js
@@ -1,27 +1,28 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
-// const withClass = (WrappedComponet, className) => {
-// 	return (props) => (
-// 		<div className={className}>
-// 			<WrappedComponet {...props} />
-// 		</div>
-// 	)
+// const withClass = (WrappedComponent, className) => {
+//     return (props) => (
+//         <div className={className}>
+//             <WrappedComponent {...props} />
+//         </div>
+//     )
 // }
 
-const withClass = (WrappedComponet, className) => {
-	const WithClass =  class extends Component{
-		render () {
-			return (
-				<div className={className}>
-					<WrappedComponet ref={this.props.forwardedRef} {...this.props} />
-				</div>
-			)
-		}
-	}
+const withClass = (WrappedComponent, className) => {
+    const WithClass = class extends Component {
+        render () {
+            return (
+                <div className={className}>
+                    <WrappedComponent ref={this.props.forwardedRef} {...this.props} />
+                </div>
+            )
+        }
+    }
 
-	return React.forwardRef((props, ref) => {
-		return <WithClass {...props} forwardedRef={ref} />
-	});
+    return React.forwardRef((props, ref) => {
+        return <WithClass {...props} forwardedRef={ref} />
+    });
 }
+
 
 export default withClass;
