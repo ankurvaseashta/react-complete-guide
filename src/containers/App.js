4b447d8d8ff14841901e3724a9da601749eb61dc
ankurvaseashta <root@USD1703230.local>
2018-08-29 11:06:44 -0400

    Fix up ref to WithClass, there is a bug in focus

--- src/containers/App.js
+++ src/containers/App.js
@@ -4,7 +4,7 @@ import classes from './App.css';
 import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
 import Aux from '../hoc/Aux';
-import withClass from '../hoc/withClass';
+import withClass from '../hoc/WithClass';
 
 class App extends PureComponent {
   constructor(props){
