5cc2c572a15fb99ebf93123893de3eac7540fccd
avaseashta <avaseash@gmail.com>
2019-09-30 14:00:50 -0400

    Add formatting to Cockpit JS

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -7,42 +7,45 @@ const cockpit = (props) => {
     ///let classes = ['red', 'bold'].join(' '); ///array of strings seperated by a empty space using join "red bold"
     const assignedClasses = [];
     let btnClass = classes.Button;
-    let btnLogin= classes.loginButton;
-    let btnLogout= classes.logoutButton;
+    let btnLogin = classes.loginButton;
+    let btnLogout = classes.logoutButton;
 
- 	if(props.showPersons){
-	  btnClass = [classes.Button, classes.Red].join(' ');
- 	}
-    if(props.persons.length <= 4){
-      assignedClasses.push( classes.green ); // classes = ['green']
+    if (props.showPersons) {
+        btnClass = [classes.Button, classes.Red].join(' ');
     }
-    if(props.persons.length <= 3){
-      assignedClasses.pop( );
-      assignedClasses.push( classes.blue ); // classes = ['blue']
+    if (props.persons.length <= 4) {
+        assignedClasses.push(classes.green); // classes = ['green']
     }
-    if(props.persons.length <= 2){
-      assignedClasses.pop(  );
-      assignedClasses.push( classes.red ); // classes = ['red']
+    if (props.persons.length <= 3) {
+        assignedClasses.pop();
+        assignedClasses.push(classes.blue); // classes = ['blue']
     }
-    if(props.persons.length <= 1){
-      assignedClasses.push( classes.bold ); // classes = ['red', 'bold']
+    if (props.persons.length <= 2) {
+        assignedClasses.pop();
+        assignedClasses.push(classes.red); // classes = ['red']
+    }
+    if (props.persons.length <= 1) {
+        assignedClasses.push(classes.bold); // classes = ['red', 'bold']
     }
 
-	return(
+    return (
         <Auxi>
-            <h1> { props.appTitle } </h1>
+            <h1> {props.appTitle} </h1>
             <p className={assignedClasses.join(' ')}> People in list! </p>
             <button
                 className={btnClass}
-                onClick={props.clicked}>Toggle Persons</button>
+                onClick={props.clicked}>Toggle Persons
+            </button>
             <button
                 className={btnLogin}
-                onClick={props.login}>Login</button>
+                onClick={props.login}>Login
+            </button>
             <button
                 className={btnLogout}
-                onClick={props.logout}>Logout</button>
+                onClick={props.logout}>Logout
+            </button>
         </Auxi>
-	);
+    );
 };
 
 export default cockpit;
