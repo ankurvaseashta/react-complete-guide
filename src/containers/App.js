ea9ddf326aa1417b4e078b334701a62d028903c0
avaseashta <avaseash@gmail.com>
2019-10-09 13:55:55 -0400

    Add type fox to persons

--- src/containers/App.js
+++ src/containers/App.js
@@ -18,7 +18,8 @@ class App extends PureComponent {
                 {id: 'abce', name: 'Helena', age: 9, type: 'Human'},
                 {id: 'abcf', name: 'Buddy', age: 5, type: 'Dog'},
                 {id: 'abcg', name: 'Valentina', age: 5, type: 'Cat'},
-                {id: 'abch', name: 'Silly', age: 7, type: 'Dog'}
+                {id: 'abch', name: 'Silly', age: 7, type: 'Dog'},
+                {id: 'abci', name: 'Crazy', age: 2, type: 'Fox'}
             ],
             otherState: 'some other value',
             showPersons: false,
